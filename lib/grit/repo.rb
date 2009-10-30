ccc96f2bf325932a8305ea9e0c545c54e05f2b23
Scott Chacon <schacon@gmail.com>
2009-10-30 12:41:48 -0700

    getting commit objects

--- lib/grit/repo.rb
+++ lib/grit/repo.rb
@@ -257,6 +257,13 @@ module Grit
       Grit.no_quote = false
       obj
     end
+
+    def commit_objects(refs)
+      Grit.no_quote = true
+      obj = self.git.rev_list({:timeout => false}, refs).split("\n").map { |a| a[0, 40] }
+      Grit.no_quote = false
+      obj
+    end
     
     def objects_between(ref1, ref2 = nil)
       if ref2
